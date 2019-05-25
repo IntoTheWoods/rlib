968da549a7b3e8ddc073560127d43f090e6aad23
bereyingas <565490+bereginyas@users.noreply.github.com>
2019-05-25 00:11:08 -0400

    commented out xlsx

--- libbase/R/xlsx_write.R
+++ libbase/R/xlsx_write.R
@@ -1,18 +1,23 @@
-library(xlsx)
+##
+## Removing xlsx dependence to allow shinyapps.io to compile this package
+##
+##
 
-xlsx.write <- function (file, ...)
-{
-  require(xlsx, quietly = TRUE)
-  objects <- list(...)
-  fargs <- as.list(match.call(expand.dots = TRUE))
-  objnames <- as.character(fargs)[-c(1, 2)]
-  nobjects <- length(objects)
-  for (i in 1:nobjects) {
-    if (i == 1)
-      write.xlsx(objects[[i]], file, sheetName = objnames[i])
-    else write.xlsx(objects[[i]], file, sheetName = objnames[i],
-                    append = TRUE)
-  }
-}
+#library(xlsx)
+#
+#xlsx.write <- function (file, ...)
+#{
+#  require(xlsx, quietly = TRUE)
+#  objects <- list(...)
+#  fargs <- as.list(match.call(expand.dots = TRUE))
+#  objnames <- as.character(fargs)[-c(1, 2)]
+#  nobjects <- length(objects)
+#  for (i in 1:nobjects) {
+#    if (i == 1)
+#      write.xlsx(objects[[i]], file, sheetName = objnames[i])
+#    else write.xlsx(objects[[i]], file, sheetName = objnames[i],
+#                    append = TRUE)
+#  }
+#}
 
 
